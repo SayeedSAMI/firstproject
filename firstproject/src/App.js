5b69d2ce857d6c938f68cb420892b29092ab4a43
Sayeedsami <sayeedsami26@gmail.com>
2024-10-19 15:45:44 +0600

    Project added and some work has been done

---
+++ firstproject/src/App.js
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+import AppHeader from './components/header';
+import AppHero from './components/hero';
+import AppAbout from './components/about';
+function App() {
+  return (
+    <div className="App">
+      <header id="header">
+        <AppHeader></AppHeader>    
+      </header>
+      <main>
+        <AppHero />
+        <AppAbout />
+      </main>
+    
+    </div>
+  );
+}
+
+export default App;
