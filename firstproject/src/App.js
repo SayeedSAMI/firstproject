419da230006b4a6765fc39590bb7c7a77963f4ee
Sayeedsami <sayeedsami26@gmail.com>
2024-10-22 11:37:58 +0600

    pricing.js added

--- firstproject/src/App.js
+++ firstproject/src/App.js
@@ -1,20 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-
-import AppHeader from './components/header';
-import AppHero from './components/hero';
-import AppAbout from './components/about';
-import AppService from './components/services';
-import AppWorks from './components/works';
-import AppTeams from './components/teams';
-import AppTestimonials from './components/testimonials';
+import AppHeader from "./components/header";
+import AppHero from "./components/hero";
+import AppAbout from "./components/about";
+import AppService from "./components/services";
+import AppWorks from "./components/works";
+import AppTeams from "./components/teams";
+import AppTestimonials from "./components/testimonials";
+import AppPricing from "./components/pricing";
 
 function App() {
   return (
     <div className="App">
       <header id="header">
-        <AppHeader></AppHeader>    
+        <AppHeader></AppHeader>
       </header>
       <main>
         <AppHero />
@@ -23,10 +23,8 @@ function App() {
         <AppWorks />
         <AppTeams />
         <AppTestimonials />
-
-
+        <AppPricing />
       </main>
-    
     </div>
   );
 }
